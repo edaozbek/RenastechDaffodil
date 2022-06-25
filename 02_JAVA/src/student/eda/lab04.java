d2d6f2fea45be75c38f3de6a4094411a612b4a46
edaozbek <edaozbek@gmail.com>
2022-06-24 20:52:45 -0400

    trial2

--- 02_JAVA/src/student/eda/lab04.java
+++ 02_JAVA/src/student/eda/lab04.java
@@ -1,4 +1,42 @@
 package student.eda;
 
 public class lab04 {
-}
+    public static void main(String[] args) {
+
+        System.out.println(uniq("AAABBBCCCDEF"));
+    }
+
+    //(String) Find the unique
+//Write a return method that can find the unique characters from the String
+//Ex: unique("AAABBBCCCDEF") ==> "DEF";
+
+
+    public static String uniq(String word) {
+
+        String result = "";
+        for (int i = 0; i < word.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < word.length(); j++) {
+
+                if (word.charAt(i) == word.charAt(j)) {
+
+                    count++;
+
+                }
+            }
+            if (count == 1) {
+
+
+                result += word.charAt(i);
+
+
+            }
+
+        }
+
+
+        return result;
+
+    }}
+
--- 02_JAVA/src/student/eda/lab06.java
+++ 02_JAVA/src/student/eda/lab06.java
@@ -1,4 +1,25 @@
 package student.eda;
 
 public class lab06 {
+
+
+    public static void main(String[] args) {
+        int [] n2={2,6,7,1,99,33};
+        min(n2);
+    }
+
+    public static void min(int[] n2){
+
+        int minnumber=n2[0];
+
+        for (int i = 0; i < n2.length; i++) {
+
+            if(n2[i]<minnumber){
+
+                minnumber=n2[i];
+            }
+        }
+        System.out.println(minnumber);
+
+    }
 }
